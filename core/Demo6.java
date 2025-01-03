510678eac0e3d36b7694bb3a71b379cdcdecead7
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-03 10:13:30 +0530

    UserCredentials2

---
+++ core/Demo6.java
@@ -0,0 +1,12 @@
+public class Demo6 {
+    public static void main(String[]args){
+        String username = "Santhosh";
+        String password = "12345";
+
+        while((username == "Santhosh") && (password == "12345")){
+                System.out.println(" Hello Santhosh,Happy New year");
+                break;
+        }
+    }
+    
+}
