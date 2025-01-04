fe2214b3cbf8213c408717175ffae4b69e71ea73
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-04 20:52:38 +0530

    Assesement-1

---
+++ core/Assesment1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Assesment1 {
+    public static void main(String[] args) {
+        Scanner s8 = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = s8.nextInt();
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+        s8.close();
+    }
+}
