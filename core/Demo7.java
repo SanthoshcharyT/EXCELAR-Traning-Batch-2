8b3cae1d09cc029ed092f4167fbeec1b4c526603
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-04 10:41:17 +0530

    UserInput Credentials

--- core/Demo7.java
+++ core/Demo7.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
+
 public class Demo7 {
-    public static void main(String[]args){
-        
-        String Name;
-        Long phoneno;
-        int age;
 
-        Scanner s1 = new Scanner(System.in);
+    public static void main(String[] args) {
 
-        System.out.print("Enter Your Name: ");
-        s1.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your PhoneNum: ");
-        s1.nextInt();
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
 
-        System.out.print("Enter Your Age: ");
-        s1.nextInt();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
 
-        s1.close();
+        scanner.nextLine();
+
+        System.out.print("Enter your phone number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Hello " + name + ", you are " + age + " years old, and your phone number is " + phoneNumber + ".");
+
+        scanner.close();
     }
-    
 }
