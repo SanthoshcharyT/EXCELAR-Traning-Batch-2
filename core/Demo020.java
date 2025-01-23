21a783ae099d1e3aea13ddf27c8a5723b906d136
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-23 21:50:43 +0530

    Biggest Number

--- core/Demo020.java
+++ core/Demo020.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class Demo_020 {
+public class Demo020 {
     public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
         System.out.print("Enter the Number: ");
