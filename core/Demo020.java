3717883ca50f95eca9fba95a5094ff8ac27cc50d
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-26 19:27:33 +0530

    FizzBuzz Program

--- core/Demo020.java
+++ core/Demo020.java
@@ -1,19 +1,21 @@
-/*Checking the given number is Even Or Odd*/
-
-
-import java.util.Scanner;
+/*FizzBuzz Program */
 
 public class Demo020 {
     public static void main(String[] args) {
-        Scanner s1 = new Scanner(System.in);
-        System.out.print("Enter the Number: ");
-        int a = s1.nextInt();
-
-        if (a % 2 == 0) {
-            System.out.println("The number " + a + " is Even");
-        } else {
-            System.out.println("the number " + a + " is Odd");
+        for(int i=0;i<=100;i++){
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz. ");
+            }else if(i%3==0){
+                System.out.println("Fizz. ");
+            }else if(i%5==0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
         }
+
+
+
     }
 
 }
