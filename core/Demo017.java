fca45ef55e056d7fd122177d1be51f34b6a93315
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-26 19:24:59 +0530

    Sum of 2 Digits

--- core/Demo017.java
+++ core/Demo017.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo017{
+public class Demo017 {
     public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
         System.out.print("Enter the First Number: ");
@@ -9,7 +9,7 @@ public class Demo017{
         System.out.print("Enter the Second Number: ");
         int num2 = s1.nextInt();
 
-        int a = num1 +num2;
+        int a = num1 + num2;
         System.out.println("The Sum of " + num1 + " and " + num2 + " is: " + a);
     }
 }
