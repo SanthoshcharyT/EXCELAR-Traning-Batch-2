33951c98edd517eb2c4f6047c6cce77218bb84b7
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-26 20:05:09 +0530

    Fibbinacci Series Using Recursion

--- core/Demo028.java
+++ core/Demo028.java
@@ -1,15 +1,22 @@
+/*Add recursive Fibonacci function with user input */
+
 import java.util.Scanner;
 
 public class Demo028 {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a lowercase character: ");
-        char lower = scanner.next().charAt(0);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
 
-        char upper = Character.toUpperCase(lower);
-
-        System.out.println("The uppercase character is: " + upper);
+        System.out.println("Fibonacci number at position " + n + ": " + fibonacci(n));
 
         scanner.close();
     }
