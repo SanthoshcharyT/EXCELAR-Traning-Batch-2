c969884b809828c199237ca0a69525a0ddde378b
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-26 19:52:58 +0530

    FizBuzz Program

--- core/Demo020.java
+++ core/Demo020.java
@@ -2,19 +2,17 @@
 
 public class Demo020 {
     public static void main(String[] args) {
-        for(int i=0;i<=100;i++){
-            if(i%3==0 && i%5==0){
+        for (int i = 0; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz. ");
-            }else if(i%3==0){
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz. ");
-            }else if(i%5==0){
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }else{
+            } else {
                 System.out.println(i);
             }
         }
-
-
 
     }
 
