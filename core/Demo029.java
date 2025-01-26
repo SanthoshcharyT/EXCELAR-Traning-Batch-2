c3653028a03c103b06afa3ec306975f7a3faecc7
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-26 20:04:00 +0530

    Array of 10 friends' names

--- core/Demo029.java
+++ core/Demo029.java
@@ -1,16 +1,21 @@
-import java.util.Scanner;
-
 public class Demo029 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String[][] friends = {
+            {"Tony"},
+            {"Sai"},
+            {"Madhav"},
+            {"Arun"},
+            {"Narendra"},
+            {"Sathwik"},
+            {"Yashwanth"},
+            {"Vineesh"},
+            {"Santhosh"},
+            {"Tony"}
+        };
 
-        System.out.print("Enter an uppercase character: ");
-        char upper = scanner.next().charAt(0);
-
-        char lower = Character.toLowerCase(upper);
-
-        System.out.println("The lowercase character is: " + lower);
-
-        scanner.close();
+        for (String[] friend : friends) {
+            System.out.println("Name: " + friend[0]);
+            System.out.println("----------------------");
+        }
     }
 }
