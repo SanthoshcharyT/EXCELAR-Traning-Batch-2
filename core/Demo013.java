5ddad2010eed29b77edd30e2ad23f080a8ed6032
SanthoshcharyT <santhoshchary0203@gmail.com>
2025-01-26 19:09:26 +0530

    Area Of Traingle

--- core/Demo013.java
+++ core/Demo013.java
@@ -1,19 +1,19 @@
+import java.util.Scanner;
+
 public class Demo013 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = -a;  
-        int c = +a;  
-        int d = ++a; 
-        int e = a++; 
-        int f = --a; 
-        int g = a--; 
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Original value of a: " + a);
-        System.out.println("Unary minus (-a): " + b);
-        System.out.println("Unary plus (+a): " + c);
-        System.out.println("Pre-increment (++a): " + d);
-        System.out.println("Post-increment (a++): " + e);
-        System.out.println("Pre-decrement (--a): " + f);
-        System.out.println("Post-decrement (a--): " + g);
+        System.out.print("Enter the base of the triangle: ");
+        double base = scanner.nextDouble();
+
+        System.out.print("Enter the height of the triangle: ");
+        double height = scanner.nextDouble();
+
+        double area = (base * height) / 2;
+
+        System.out.printf("The area of the triangle is: %.2f%n", area);
+
+        scanner.close();
     }
 }
