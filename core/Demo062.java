ece24ef9d02c09d59df0461a2ee312b23b145b74
SanthoshChary <santhoshchary0203@gmail.com>
2025-02-05 10:16:42 +0530

    first, second, and last elements of an array

---
+++ core/Demo062.java
@@ -0,0 +1,14 @@
+//Print the first, second, and last elements of an array  
+
+
+public class Demo062 {
+    public static void main(String[] args) {
+        int array[] = {1,8,6,4,5,9,7};
+        int n = array.length-1;
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[n-1]);
+    }
+
+}
