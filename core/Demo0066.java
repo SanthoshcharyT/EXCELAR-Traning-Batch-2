a66e3d5a242b22bf21ff4f746ab793e3c473ce6f
SanthoshChary <santhoshchary0203@gmail.com>
2025-02-13 10:28:57 +0530

    index values of an array

---
+++ core/Demo0066.java
@@ -0,0 +1,10 @@
+//Print index values of an array
+
+public class Demo0066 {
+    public static void main(String[] args) {
+        int arr[] = { 2, 5, 4, 9, 3 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("index Value " + i + " = " + arr[i] + " ");
+        }
+    }
+}
