cfe0a0ff1a6b5aeda1e35c292fe74844cc3fc85e
SanthoshChary <santhoshchary0203@gmail.com>
2025-02-16 13:16:28 +0530

    Copy array elements into another array

---
+++ core/Demo077.java
@@ -0,0 +1,18 @@
+//Copy array elements into another array
+
+public class Demo077 {
+    public static void main(String[] args) {
+        int arr1[] = {6,5,4,3,2,1,9,8,7};
+        int n = arr1.length;
+        int arr2[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr2[i] = arr1[i];
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr2[i] + " ");
+        }
+    }
+    
+}
